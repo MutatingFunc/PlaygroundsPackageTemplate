da3dbaafb9b02e332f4737f5a1aaf6cf964731f2
James Froggatt <james.froggatt@me.com>
2024-09-08 17:12:52 +0100

    Initial commit

---
+++ Template/PackageView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+public struct PackageView: View {
+    
+    public init() {
+        
+    }
+    
+    public var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    PackageView()
+}
---
+++ Template_App/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import Template
+
+struct ContentView: View {
+    var body: some View {
+        PackageView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
